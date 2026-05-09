12674fabb418605c9443b3919694ef19921279c9
Grintaking19 <omarbadrh.ob@gmail.com>
2026-05-09 20:38:36 +0300

    Finished setting up validation schema for user model

--- auth-service/src/controllers/auth-controller.js
+++ auth-service/src/controllers/auth-controller.js
@@ -1,16 +1,19 @@
- 
-
-
+import logger from "../utils/logger.js";
 
 // User Registration
-
-
+const registerUser = async (req, res) => {
+  // Implementation for user registration
+  logger.info("Registration endpoint hit...");
+  try {
+    
+  } catch (error) {}
+};
 
 // User Login
-
-
+const loginUser = async (req, res) => {
+  // Implementation for user login
+};
 
 // Refresh Token
-
 
 // Logout
