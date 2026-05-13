fb49059ee00888b86cd85240060d07db8d3d9c11
Grintaking19 <omarbadrh.ob@gmail.com>
2026-05-13 15:24:54 +0300

    Added logger and handleError to api-gateway service

---
+++ api-gateway/src/utils/logger.js
@@ -0,0 +1,44 @@
+import winston from "winston";
+
+const customLevels = {
+  levels: {
+    critical: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    debug: 4,
+  },
+  colors: {
+    critical: "red",
+    error: "red",
+    warn: "yellow",
+    info: "green",
+    debug: "blue",
+  },
+};
+
+const logger = winston.createLogger({
+  levels: customLevels.levels,
+  level: process.env.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json(),
+  ),
+  defaultMeta: { service: "api-gateway" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+      ),
+    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+winston.addColors(customLevels.colors);
+
+export default logger;
